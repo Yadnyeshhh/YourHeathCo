75c68a02b0b81751151d7f2c3251a2173955519c
Yadnyeshhh <yadnyesh1024@gmail.com>
2025-07-14 21:50:02 +0530

    Update Login.jsx

--- my-app/src/pages/Login.jsx
+++ my-app/src/pages/Login.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import "../stylesheets/login.css";
+import "../stylesheets/Login.css";
 
 const apiUrl = import.meta.env.VITE_API_URL;
 
